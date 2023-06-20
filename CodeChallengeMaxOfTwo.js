ccdf4d6bd4bc5d822a25a41369bb028b4a982679
Katryna Yaworski <Katrynayaworski@gmail.com>
2023-06-20 13:17:55 -0500

    Soltution Complete

---
+++ CodeChallengeMaxOfTwo.js
@@ -0,0 +1,24 @@
+/*Given the starting code of:
+
+x = 1
+y = 2
+Write a block of code that will print out the largest value.
+
+Using the given values as an example, your code should print out 2.
+
+To verify your code is working properly, try and change the values of x and y.
+
+As an additional challenge, print out The values are identical. if x and y have the same value.
+*/
+
+let x = 5
+let y = 5
+
+function MaxOfTwo(a, b){
+    if(x > y){
+        return x
+    }else if(y > x){
+        return y
+    } else return (`both ${a} and ${b} are equal`)
+}
+console.log(MaxOfTwo(x, y))
